9ee062a9dda0a5a5b606613f94b89e7807338a3f
hwangbj123 <bj09289612@gmail.com>
2023-10-24 12:02:29 +0900

    first

---
+++ src/main/java/com/green/greenstock/GreenStockApplication.java
@@ -0,0 +1,13 @@
+package com.green.greenstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/green/greenstock/GreenStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.greenstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
