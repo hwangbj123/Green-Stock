e9fc50c4fa614844803e947a1355da7103312416
Maleeang <121776914+Maleeang@users.noreply.github.com>
2023-11-07 08:25:15 +0900

    환불 기능

---
+++ src/main/java/com/green/greenstock/repository/interfaces/PayRepository.java
@@ -0,0 +1,12 @@
+package com.green.greenstock.repository.interfaces;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.green.greenstock.repository.model.Pay;
+
+@Mapper
+public interface PayRepository {
+	public int insertPayInfo(Pay pay);
+
+	public int modifyPayInfo(Pay pay);
+}
