41f5e1ce6722f33216cfa7409d1187ca7e21e9ab
hwangbj123 <bj09289612@gmail.com>
2023-11-07 09:55:05 +0900

    domestic_stock_code 테이블,코드 수정

--- src/main/webapp/resources/js/custom/boardList.js
+++ src/main/webapp/resources/js/custom/boardList.js
@@ -3,6 +3,12 @@ let boardListInit = {
 	init: function(){
 		this.keepCategory();
 		this.nowCategory();
+		document.getElementById('search-word').addEventListener('keypress', (event) => {
+		    if (event.key === "Enter") {
+		        event.preventDefault();
+		        this.searchValid();
+		    }
+		});
 	},
 	toSignIn: function(){
 		if(confirm("로그인이 필요한 서비스입니다\n로그인 화면으로 이동하시겠습니까?")){
@@ -11,10 +17,17 @@ let boardListInit = {
 	},
 	
 	searchValid: function(){
-		if(document.getElementById("search-word").value.length!=0){
-			document.getElementById("search-frm").submit();
+		let word = document.getElementById("search-word");
+		if(word.value.trim().length==0){
+			word.value = "";
+			word.focus();
+		}else if(/<|>/.test(word.value)){
+			alert("일부 특수문자는 사용할 수 없습니다");
+    		word.value="";
+    		word.focus();
 		}else{
-			document.getElementById("search-word").focus();
+		console.log('word - if3');
+			document.getElementById("search-frm").submit();
 		}
 	},
 	
@@ -39,10 +52,14 @@ let boardListInit = {
 	},
 	
 	nowCategory: function(){
-		console.log('now Category method');
 		let cate = this.searchParam('categoryId');
-		console.log('cate : '+cate);
-		document.getElementById('category-btn-'+cate).style.backgroundColor = "#eee";
+	    let categoryButton = document.getElementById('category-btn-' + cate);
+		console.log('categoryButton : '+categoryButton)
+	    if (categoryButton) {
+	        categoryButton.style.backgroundColor = "#f7f7f7";
+	    } else {
+	        console.log("해당 ID를 가진 카테고리 버튼을 찾을 수 없습니다.");
+	    }
 	},
 };
 
